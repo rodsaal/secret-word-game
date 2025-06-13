c72b446f97dc398e050e105e090f27de701875f6
rodrigo almeida <rodrigo.rsaal@gmail.com>
2025-06-13 11:44:40 -0300

    Finishing the game

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,9 +1,12 @@
 import './GameOver.css'
 
-const GameOver = ({retry}) => {
+const GameOver = ({ retry, score }) => {
   return (
     <div>
-      <h2>Game Over</h2>
+      <h2>Fim de Jogo!</h2>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>
+      </h2>
       <button onClick={retry}>Recomeçar</button>
     </div>
   );
